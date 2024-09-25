934bd5247fc5cf66e6fa934ef2cc8552f11fc302
sourav-maji <s722143@gmail.com>
2024-09-25 18:36:27 +0530

    method overloading

--- Fourth.java
+++ Fourth.java
@@ -11,6 +11,22 @@ class Student{
     
 }
 
+// method overloading
+
+class Calculate{
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+
+    public int add(int n1, int n2, int n3){
+        return n1+n2+n3;
+    }
+
+    public double add(int n1, double n2){
+        return n1+n2;
+    }
+}
+
 public class Fourth {
    static Scanner sc = new Scanner(System.in);
     public static void main(String args[]){
@@ -23,6 +39,10 @@ public class Fourth {
 
         System.out.println(name.toUpperCase() + " has obtained "+ marks1 + " in the exam");
         
+        Calculate cal = new Calculate();
+        System.out.println(cal.add(6, 10));
+        System.out.println(cal.add(6, 10,15));
+        System.out.println(cal.add(6, 10.5));
 
     }
     
