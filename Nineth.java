a938d436a0525ea1fc192364699501b9d92fd82c
sourav-maji <s722143@gmail.com>
2024-09-29 19:51:27 +0530

    Implementation of equal method in object.

--- Nineth.java
+++ Nineth.java
@@ -6,17 +6,31 @@ class Laptop {
         return "model" + " : " +price;
     }
 
+    public boolean equal(Laptop that){
+        if((this.model == that.model) && (this.price == that.price)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
 
 public class Nineth {
     
     public static void main(String[] args) {
-        Laptop obj = new Laptop();
-        obj.model = "MacBook";
-        obj.price =3500;
+        Laptop obj1 = new Laptop();
+        obj1.model = "MacBook";
+        obj1.price =3500;
 
-        System.out.println(obj); // Laptop@6b95977
-        System.out.println(obj.toString()); // Laptop@6b95977   
+        System.out.println(obj1); // Laptop@6b95977
+        System.out.println(obj1.toString()); // Laptop@6b95977   
         // the obj call by default calling the toString() method
+
+        Laptop obj2 = new Laptop();
+        obj2.model = "MacBook";
+        obj2.price =3500;
+        boolean result = obj1.equal(obj2);
+        System.out.println(result);
     }
 }
