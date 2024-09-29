3ba4580d58a5bfdee3698c92852b5333351c9637
sourav-maji <s722143@gmail.com>
2024-09-29 19:35:41 +0530

    final keyword in method and class

--- Eight.java
+++ Eight.java
@@ -16,6 +16,23 @@ class B extends A {
 //     }
 // }
 
+final class Calc{
+    final public void show(){
+        System.out.println("In Calc show");
+
+        // we use final keyword in method to prevent the method over-riding 
+    }
+
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+}
+
+// class AdvCalc extends Calc{
+    // this will give an error as we can't extend the final class.
+    // we use final keyword for class to stop the inheritance of the class
+// }
+
 public class Eight {
    public static void main(String[] args) {
         B obj = new B();
@@ -24,5 +41,9 @@ public class Eight {
         // final :- we if use final keyword then we can't change the value
         final int num =9;
         // num =10; // this will thow an error as we can't change value of final variable 
+
+        Calc obj1 =new Calc();
+        obj1.show();
+        obj1.add(3,7);
    } 
 }
