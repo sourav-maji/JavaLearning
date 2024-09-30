0e25d14d24a287c7fe66bbfcc6ab15ea05ba13c2
sourav-maji <s722143@gmail.com>
2024-09-30 19:46:28 +0530

    Set in Java

--- Sixteen.java
+++ Sixteen.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Sixteen {
     public static void main(String[] args) {
@@ -13,5 +16,26 @@ public class Sixteen {
         for(int n: nums){
             System.out.println(n);
         }
+
+
+        Set<Integer> num = new HashSet<Integer>(); 
+        // hashset doesn't return duplicate value and it doesn't return elements in sorted order
+        num.add(5);
+        num.add(5);
+        num.add(7);
+        num.add(2);
+        num.add(3);
+
+        System.out.println(num);
+
+        Set<Integer> num1 = new TreeSet<Integer>(); 
+        // TreeSet doesn't return duplicate value and it returns the elements in sorted order
+        num1.add(5);
+        num1.add(5);
+        num1.add(7);
+        num1.add(2);
+        num1.add(3);
+
+        System.out.println(num1);
     }
 }
